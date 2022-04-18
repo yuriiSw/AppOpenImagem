1b5fc183880d4088d0df9ebba1703ba9168fe309
yuri.wsouza <yuri.wsouza@SBC0505213W10-1>
2022-04-18 09:54:15 -0300

    Adicionar arquivos de projeto.

---
+++ Gameplay/Home.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Gameplay
+{
+    partial class Home
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gameplaybotao = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GameplayOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gameplaybotao);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(126, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // gameplaybotao
+            // 
+            this.gameplaybotao.Location = new System.Drawing.Point(119, 321);
+            this.gameplaybotao.Name = "gameplaybotao";
+            this.gameplaybotao.Size = new System.Drawing.Size(234, 23);
+            this.gameplaybotao.TabIndex = 1;
+            this.gameplaybotao.Text = "BOTAO ANIMADO";
+            this.gameplaybotao.UseVisualStyleBackColor = true;
+            this.gameplaybotao.Click += new System.EventHandler(this.gameplaybotao_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 358);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GameplayOpenFile
+            // 
+            this.GameplayOpenFile.FileName = "openFileDialog1";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog GameplayOpenFile;
+        private System.Windows.Forms.Button gameplaybotao;
+
+    }
+}
+
---
+++ Gameplay/Home.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gameplay
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void gameplaybotao_Click(object sender, EventArgs e)
+        {
+            GameplayOpenFile.Filter = "Arquivos bmp | *.bmp |Arquivos jpg | *.jpg";
+            if(GameplayOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = GameplayOpenFile.FileName;
+            }
+        }
+    }
+}
